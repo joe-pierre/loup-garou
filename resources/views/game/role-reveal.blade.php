371da7e05103aebb03cbe53c2dbf5b5e826f94db
joe-pierre <gomisjoepierre@gmail.com>
2026-06-23 20:27:02 +0000

    wip toast comportements Remplacer chaque occurrence de __internalNavigation par sessionStorage

--- resources/views/game/role-reveal.blade.php
+++ resources/views/game/role-reveal.blade.php
@@ -261,7 +261,7 @@
                 clearInterval(this._gameTick);
                 gsap.killTweensOf('#game-timer-fill');
                 gsap.killTweensOf('#reveal-timer-bar');
-                window.__internalNavigation = true;
+                sessionStorage.setItem('__internalNavigation', '1');
                 window.location.href = '/game/{{ $game->code }}/mayor-election';
             },
 
@@ -285,7 +285,7 @@
                         'finished': '/game/{{ $game->code }}/finished',
                     };
                     const target = targets[data.phase];
-                    if (target && window.location.pathname !== target) { window.__internalNavigation = true; window.location.href = target; }
+                    if (target && window.location.pathname !== target) { sessionStorage.setItem('__internalNavigation', '1'); window.location.href = target; }
                 } catch { }
             },
 
